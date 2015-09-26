daa0e44be2aa322e8fe98fa0ef56d08757113585
c4rlosc7 <camartinez7@hotmail.com>
2015-09-25 21:02:05 -0500

    implementando clases

--- client.cc
+++ client.cc
@@ -24,68 +24,69 @@ void playermusica(string string_in, string usr){
 }
 
 int main(){
-  cout << "CLIENTE-MUSIC-PLAYER" << endl;
+  //cout << "CLIENTE-MUSIC-PLAYER" << endl;
   context ctx;
 
   socket s(ctx, socket_type::xreq);
   s.connect("tcp://localhost:5555");
   poller p;
   p.add(s, poller::poll_in);
-  
   string username;
-  message m;
-  cout << "nombre usuario: ";
-  cin >> username;  
+  cout << "Ingrese el nombre de usuario: ";
+  cin >> username;
   int c;
-  int estado = 1;
-	while(true)
+  string keyword;
+  string keysong;
+  
+  message m;                 // mensaje m
+while(true)
+{
+	cout << "-----------------CLIENT-MUSIC-PLAYER-----------------"	<< endl;
+	cout << "1.Buscar  2.Reproducir : ";
+	cin >> c;
+
+	switch(c)
 	{
-		while(estado == 1)
-		{
-			cout << "MUSIC-PLAYER "	<< endl;
-			cout << "1.Buscar 2.Reproducir 0.Resultado(buscar-reproducir): ";
-			cin >> c;
+		case 1: 			  	
+  			cout << "-----------------Buscar-----------------" << endl;
+  			
+  			cout << "palabra a buscar: ";
+  			cin >> keyword;  
 
-			switch(c)
-			{
-				case 1: 		                        // m [id,"buscar",number]	  	
-					int number;  
-					cout << "Ingrese el indice a buscar solo tenemos 1-5: ";
-					cin >> number;
-					m << "buscar";
-					m << number;
-					s.send(m);
-					break;
-					
-				case 2:                                  // m [id,"reproducir",number]
-					int index_songc;				
-					cout << "Entre el indice de la cancion que desea: "; 
-					cin >> index_songc;	
-					m << "reproducir";
-					m << index_songc;
-					s.send(m);	
-					break;		
+  			m << c;
+  			m << keyword;
+  			s.send(m);	                  
+			break;            // id, 1, keyword
 
-				case 0:
-					if (p.poll()){
-						if (p.has_input(s)){
-							message rs;                     // receive string with message answertNSong
-							s.receive(rs);	
-							string resultFinal;
-							rs >> resultFinal;
-							//cout << resultFinal.size();
-							if (resultFinal.size() < 40){		
-								cout << "Nombre de la cancion: " << resultFinal << endl;
-							}else{
-								playermusica(resultFinal, username);
-							}							
-						}
-					}
+		case 2:                           
+			cout << "-----------------Reproducir-----------------" << endl;	
+				
+			cout << "Nombre cancion: "; 
+			cin >> keysong;	
+			m << c;
+			m << keysong;
+			s.send(m);	      // id, 2, keysong
+			break;	
 
-					break;
-			}
+		default: cout << "Ingrese opcion correcta";
+    		break;															
+	}		
 
+  message r;
+  s.receive(r);
+  string tarea;
+  r >> tarea;  
+  string stringsong;
+  r >> stringsong;
+  cout << "Canciones encontradas: " << r.parts() << endl;
+
+  if(tarea == "lista"){
+	  for(size_t i = 0; i < r.parts(); i++) {
+			cout << r.get(i) << endl;
 		}
-	}
+  }else if(tarea == "reproducir"){
+  	playermusica(stringsong, username);
+  }
+}
   return 0;
 }
--- server.cc
+++ server.cc
@@ -2,15 +2,15 @@
 #include <iostream>  // std::cout
 #include <fstream>  //std::ifstream
 #include <string>
+#include <list>
 #include <cassert>
 #include <zmqpp/zmqpp.hpp>
-
 using namespace std;
 using namespace zmqpp;
-
+/*
 //////////////////////////////// find to name song 
-string loadToNameSong(int a){
-
+map<int, string> getList()
+{
 	map<int,string> map_name;
 	fstream inputFile;
 	int indx=0;
@@ -23,19 +23,17 @@ string loadToNameSong(int a){
 								
 				getline(inputFile , nombre);
 				map_name[indx]=nombre;					
-				cout<< indx << " | " << nombre << endl;			
+				//cout<< indx << " | " << nombre << endl;			
 				indx++;
 			}
 			inputFile.close();
 		}else cout << "Fichero inexistente" << endl;
-	std::map<int, string>::iterator it=map_name.find(a); // buscar
-	name_song = (*it).second;
-	return name_song;
+	return map_name;
 }	
 
 //////////////////////////////// find to path
-string findToPath(int b){
-
+string getSong(int b)
+{
 	map<int,string> map_path;
 	fstream inputFile;
 	int indx=0;
@@ -48,7 +46,6 @@ string findToPath(int b){
 								
 				getline(inputFile , nombre);
 				map_path[indx]=nombre;					
-				cout<< indx << " | " << nombre << endl;			
 				indx++;
 			}
 			inputFile.close();
@@ -57,7 +54,88 @@ string findToPath(int b){
 	path = (*it).second;
 	return path;
 }	
+*/
+/////////////////CLASS-SONG-OBJECT-// name, artist, filename, Metodo string Getname
+class song{
 
+	private:
+		string name;
+		string artist;
+		string filename;
+	public:	
+		song(){}  
+		song(const string &name1, const string &artist1, const string &filename1){			
+			name = name1;			
+			artist = artist1;
+			filename = filename1;
+		}
+		const string& getname() const { 
+			return name; 
+		}		
+		const string& getartistic() const { 
+			return artist; 
+		}
+		const string& getfilename() const { 
+			return filename; 
+		}				
+};
+/////////////////CLASS-PLAYLIST-OBJECT-// lista de canciones name, Metodo add,size
+class playlist{
+	private:
+		list <song> songs;  // lista de objetos de tipo song 
+		string name;		
+	public:		
+		playlist(){}
+		playlist(const string &n){ 
+			name = n;
+		}		
+		void add(song s){ //Add element at the end
+		songs.push_back(s);
+		}		
+		int size(){ // Returns the number of elements on list
+		return songs.size();
+		}				
+};
+/////////////////CLASS-PLAYER-OBJECT-// mylist Metodo addsong
+class player{
+
+	private:
+		list <song> all;
+		list <playlist> mylists; // lista de obj de tipo playlist 
+
+	public:
+		player(){}
+		list <song> find(string name){ //funcion de tipo list <song> llamada find() all songs
+			list <song> result;// create to mylist
+			for(const song& s: all){
+				auto p = s.getname().find(name); 	
+				//cout << s.getname() << endl;
+				if (p != string::npos){ 
+					result.push_back(s);					
+					//cout << s.getname() << endl;
+				}
+			}
+			return result;         // retorna la lista de las canciones con la similitud de la busqueda
+		}
+/*
+		list <song> findtofilename(string name){ //funcion de tipo list <song> llamada find() all songs
+			list <song> result;// create to mylist
+			for(const song& s: all){
+				auto p = s.getfilename().find(name); 	
+				//cout << s.getname() << endl;
+				if (p != string::npos){ 
+					result.push_back(s);					
+					//cout << s.getname() << endl;
+				}
+			}
+			return result;         // retorna la lista de las canciones con la similitud de la busqueda
+		}						
+*/		
+		//Add element at the end
+		void addsong(song s){ 
+			all.push_back(s);			
+		}	
+};
 ////////////////////////////transfer to file 
 vector<char> ReadAllBytes(const string &filename)
 {
@@ -67,23 +145,8 @@ vector<char> ReadAllBytes(const string &filename)
 
     ifs.seekg(0, ios::beg); // stay begin file 
     ifs.read(&result[0], pos); //how much to read 
-
-    int bytes = 100 * 1024; //102400    
-    int interSize = 0;    
-
-    for (int i = 2; bytes < pos; i++)		
-    {
-    	vector<char>  part(bytes);
-	    ifs.seekg(interSize, ios::beg); // stay begin file, beginning of the stream
-	    ifs.read(part.data(), bytes); //read     	
-		interSize = ifs.gcount(); //Get character count 102400 -      		
-		bytes = interSize * i;	   
-		cout << interSize; 
-		return part;
-    }        
-    //return result;
+    return result;
 }
-
 void filetomessage(const string &filename, message &r){
 	vector <char> bytes = ReadAllBytes(filename);
 	r.add_raw(bytes.data(), bytes.size());
@@ -92,44 +155,75 @@ void filetomessage(const string &filename, message &r){
 int main()
 {
   cout << "SERVER-MUSIC-PLAYER" << endl;
+  song a("Milagro", "A.N.I.M.A.L", "/home/carlos/Escritorio/musica/0_Milagro.ogg"); 
+  song b("Familia Es la Oportunidad", "A.N.I.M.A.L", "/home/carlos/Escritorio/musica/1_Familia_Es_la_Oportunidad_.ogg");
+  song c("Los Que Marcan el Camino", "A.N.I.M.A.L", "/home/carlos/Escritorio/musica/2_Los_Que_Marcan_el_Camino.ogg");
+  song d("Poder Latino", "A.N.I.M.A.L", "/home/carlos/Escritorio/musica/3_Poder_Latino.ogg");
+  song e("Loco Pro", "A.N.I.M.A.L", "/home/carlos/Escritorio/musica/4_Loco_Pro.ogg");
+  song f("Dejar de Ser", "A.N.I.M.A.L", "/home/carlos/Escritorio/musica/5_Dejar_de_Ser.ogg");
+  playlist rock("rock"); // playlist rock 
+  rock.add(a);   //adicionar obj tipo song a la lista rock
+  rock.add(b);
+  rock.add(c);
+  rock.add(d);
+  rock.add(e);
+  rock.add(f);
+  player p;    // lista de obj playlist p
+  p.addsong(a);
+  p.addsong(b);
+  p.addsong(c);
+  p.addsong(d);
+  p.addsong(e);
+  p.addsong(f);
+  
   context ctx;
-
   socket s(ctx, socket_type::xrep);
   s.bind("tcp://*:5555");
 	
-	while(true){ 
-
+	while(true)
+	{ 
 		message r;
-		s.receive(r);       // r [id, "buscar" o "reproducir" ,number]
-
-		string id;          // id
+		s.receive(r);
+		string id;        // id 
 		r >> id;
-
-		string operation;  // operation (buscar)(reproducir)
-		r >> operation;   
-
-		int n;             // number to search
-		r >> n;               
-
-		string text_server;  // nombre de la cancion / name to song 
-		string name_path;    // ruta / path 
+		int operador;     // 1 buscar, 2 reproducir   
+		r >> operador;
 		message sc;
-		if (operation == "buscar"){
 
-			text_server = loadToNameSong(n);
-			cout << text_server << endl;
-			
+		if(operador == 1){             // 1 message [id,"lista",name-song]
+			string wordtofind;
+			r >> wordtofind;
+			list <song> s1 = p.find(wordtofind);				
 			sc << id;
-			sc << text_server;
-			s.send(sc);		
-
-		}else if(operation == "reproducir"){
-			name_path = findToPath(n);    // path to file 
-			cout << name_path << endl;
+			sc << "lista";
+			for(const song& ss: s1){
+				sc << ss.getname();
+			}
+		  	int cap;  		  		
+		  	cap = s1.size();		
+		  	cout << "Canciones encontradas: " << cap << endl;  		  		
+		  	cout << "Partes: " << sc.parts() << endl;
+		  	s.send(sc);
+		}else if(operador == 2){       // 2 message [id,"reproducir",namesong,string bytes]
+			string word;
+			r >> word;
+			list <song> s1 = p.find(word);				
 			sc << id;
-			filetomessage(name_path,sc);
-			s.send(sc);
+			sc << "reproducir";
+			string pathfile;
+			for(const song& ss: s1){
+				pathfile = ss.getfilename();
+				sc << ss.getname();
+			}
+		  	//int cap;  		  				  	
+		  	//cap = s1.size();		
+		  	cout << "ruta: " << pathfile << endl;  
+		  	filetomessage(pathfile,sc);
+			s.send(sc);		  	
+		  	//cout << "Partes: " << sc.parts() << endl;
+		  	//s.send(sc);			
 		}		
+
   }	
 	return 0;
 }
